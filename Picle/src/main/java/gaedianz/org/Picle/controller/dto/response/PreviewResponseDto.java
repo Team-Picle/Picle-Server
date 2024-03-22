1a9012279acecaede5fc5d2ee8cd58f253576855
jelliijoa <jelliijoa@gmail.com>
2024-03-22 20:41:53 +0900

    [FIX] PreviewResponseDto에서 userId 반환하도록 변경

--- Picle/src/main/java/gaedianz/org/Picle/controller/dto/response/PreviewResponseDto.java
+++ Picle/src/main/java/gaedianz/org/Picle/controller/dto/response/PreviewResponseDto.java
@@ -15,12 +15,13 @@ import java.util.Set;
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class PreviewResponseDto {
     private Long routineId;
+    private Long userId;
     private String content;
     private LocalTime time;
     private LocalDate startRepeatDate;
     private Set<DayOfWeek> repeatDays;
 
-    public static PreviewResponseDto of(Long routineId, String content, LocalTime time, LocalDate startRepeatDate, Set<DayOfWeek> repeatDays) {
-        return new PreviewResponseDto(routineId, content, time, startRepeatDate, repeatDays);
+    public static PreviewResponseDto of(Long routineId, Long userId, String content, LocalTime time, LocalDate startRepeatDate, Set<DayOfWeek> repeatDays) {
+        return new PreviewResponseDto(routineId, userId, content, time, startRepeatDate, repeatDays);
     }
 }
